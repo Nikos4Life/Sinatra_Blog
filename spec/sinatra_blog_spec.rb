ec9252ccebdc4c06bf30469a0bf0c837d09cb5e5
Miguel <mmmx3m@gmail.com>
2015-11-26 13:31:41 +0100

    First iterarion completed

--- spec/sinatra_blog_spec.rb
+++ spec/sinatra_blog_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
+require_relative("../lib/sinatra_blog/Blog.rb")
+
 
 describe SinatraBlog do
   it 'has a version number' do
     expect(SinatraBlog::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+end
+describe "BlogTest" do
+  describe "shows Blog's post" do
+    it "It shows as many post as has been created" do
+      blog1 = Blog.new()
+      blog1.add_post("How to build a Blog", DateTime.now, "Hola0", false)
+      blog1.add_post("How to build a Dog", DateTime.now, "Hola1", false)
+      array_posts = blog1.container
+      expect(array_posts.count).to eq(2)
+    end
   end
 end
